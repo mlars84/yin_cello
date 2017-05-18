17703c1dc937210204c1ad8c8b0d9fe93eaa5703
Matt Larson <matt.a.larson@gmail.com>
2017-05-18 11:59:07 -0500

    some styling and ng-route started

--- public/scripts/yinCello.js
+++ public/scripts/yinCello.js
@@ -1,3 +1,16 @@
 var myApp = angular.module('myApp', ['ngRoute'], function() {
-  
-});
+
+  myApp.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+      template: '<p>Home</p>',
+      controller: 'DefaultController as dc'
+    }).when('/events', {
+      templateUrl: '/views/pages/events.html',
+      controller: 'EventsController as ec'
+    }).when('/contact', {
+      templateUrl: '/views/pages/contact.html',
+      controller: 'ContactController as cc'
+    }).otherwise('/');
+    $locationProvider.html5Mode(true);
+  }); //end config
+}); //end angular module
